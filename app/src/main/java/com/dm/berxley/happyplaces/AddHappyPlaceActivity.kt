f34286a26ff384ebc9c82a470407c713c73841e8
Dennis Muoki <muokid3@gmail.com>
2024-05-30 11:24:51 +0300

    date picker

--- app/src/main/java/com/dm/berxley/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/dm/berxley/happyplaces/AddHappyPlaceActivity.kt
@@ -1,15 +1,26 @@
 package com.dm.berxley.happyplaces
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var toolbar: Toolbar? = null
+    private var etDate: EditText? = null
+    private  var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
         toolbar = findViewById(R.id.tool_bar_add_place)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -17,5 +28,31 @@ class AddHappyPlaceActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        etDate = findViewById(R.id.et_date)
+        etDate?.setOnClickListener(this)
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+            val format = "dd/MM/yyyy"
+            val sdf = SimpleDateFormat(format, Locale.getDefault())
+            etDate?.setText(sdf.format(cal.time).toString())
+        }
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date -> {
+                DatePickerDialog(this,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
     }
 }
